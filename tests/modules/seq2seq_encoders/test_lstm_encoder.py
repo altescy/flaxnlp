ab56fbb06e3151b86bb9def9852efff833b5508b
altescy <altescy@fastmail.com>
2023-02-03 23:08:19 +0900

    fix argument name

--- tests/modules/seq2seq_encoders/test_lstm_encoder.py
+++ tests/modules/seq2seq_encoders/test_lstm_encoder.py
@@ -6,7 +6,7 @@ from flaxnlp.util import sequence_mask
 
 
 def test_lstm_encoder() -> None:
-    encoder = LSTMEncoder(hidden_size=4, num_layers=2, bidirectional=True)  # type: ignore[no-untyped-call]
+    encoder = LSTMEncoder(hidden_dim=4, num_layers=2, bidirectional=True)  # type: ignore[no-untyped-call]
 
     inputs = numpy.random.RandomState(0).normal(size=(2, 3, 4))
     mask = sequence_mask(numpy.array([2, 3], dtype=numpy.int32))
