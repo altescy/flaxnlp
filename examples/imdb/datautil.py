5239055982bd61ed298d55a5ca7b20ab0b3b3446
altescy <altescy@fastmail.com>
2023-02-04 23:21:30 +0900

    use collatable.Collator

--- examples/imdb/datautil.py
+++ examples/imdb/datautil.py
@@ -9,11 +9,6 @@ from collatable.extras.dataset import Dataset
 from collatable.extras.indexer import TokenIndexer
 
 
-class Collator:
-    def __call__(self, instances: Sequence[Instance]) -> Dict[str, Any]:
-        return collatable.collate(instances)  # type: ignore[no-any-return]
-
-
 class TokenIndexerBuilder(colt.Registrable):  # type: ignore[misc]
     def __call__(self, documents: Iterable[Sequence[str]]) -> TokenIndexer[str]:
         raise NotImplementedError
