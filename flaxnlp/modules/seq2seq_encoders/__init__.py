d37196f85230ccf5e01f8670d91b5fdf893ffcfe
altescy <altescy@fastmail.com>
2023-02-05 02:40:48 +0900

    import modules

---
+++ flaxnlp/modules/seq2seq_encoders/__init__.py
@@ -0,0 +1,3 @@
+from flaxnlp.modules.seq2seq_encoders.lstm_encoder import LSTMEncoder  # noqa: F401
+from flaxnlp.modules.seq2seq_encoders.seq2seq_encoder import Seq2SeqEncoder  # noqa: F401
+from flaxnlp.modules.seq2seq_encoders.transformer_encoder import TransformerEncoder  # noqa: F401
---
+++ flaxnlp/modules/token_embedders/__init__.py
@@ -0,0 +1,3 @@
+from flaxnlp.modules.token_embedders.embedding import Embedding  # noqa: F401
+from flaxnlp.modules.token_embedders.pretrained_transformer_embedder import PretrainedTransformerEmbedder  # noqa: F401
+from flaxnlp.modules.token_embedders.token_embedder import TokenEmbedder  # noqa: F401
