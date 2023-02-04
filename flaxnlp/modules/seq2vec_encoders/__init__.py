356940ca08a10fecd8d36b89b3737b50bb3a1b48
altescy <altescy@fastmail.com>
2023-02-05 03:56:44 +0900

    fix module name

--- flaxnlp/modules/seq2vec_encoders/__init__.py
+++ flaxnlp/modules/seq2vec_encoders/__init__.py
@@ -1,3 +1,3 @@
-from flaxnlp.modules.seq2vec_encoders.cnn_encoder import CNNEncoder  # noqa: F401
+from flaxnlp.modules.seq2vec_encoders.cnn_encoder import CnnSeq2VecEncoder  # noqa: F401
 from flaxnlp.modules.seq2vec_encoders.self_attentive_encoder import SelfAttentiveEncoder  # noqa: F401
 from flaxnlp.modules.seq2vec_encoders.seq2vec_encoder import Seq2VecEncoder  # noqa: F401
